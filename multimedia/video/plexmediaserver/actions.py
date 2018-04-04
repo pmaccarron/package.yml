cee84b869327c143c8beede4aee3bf7e46ef30de
pmaccarron <padraig_mac@hotmail.com>
2018-04-04 16:45:45 +0100

    Update actions.py

--- multimedia/video/plexmediaserver/actions.py
+++ multimedia/video/plexmediaserver/actions.py
@@ -12,13 +12,13 @@ def setup():
     shelltools.system("ar xf plexmediaserver_%s-*_amd64.deb" % get.srcVERSION())
     shelltools.system("tar xvf data.tar.*")
     shelltools.system("sed -i 's/x-www-browser/xdg-open/' usr/share/applications/plexmediamanager.desktop")
-    shelltools.system("wget https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/plexmediaserver/files/plexmediaserver.conf")
+    shelltools.system("wget https://raw.githubusercontent.com/pmaccarron/package.yml-files/master/multimedia/video/plexmediaserver/files/plexmediaserver.conf")
 
-    shelltools.system("wget https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/plexmediaserver/files/plexmediaserver.tmpfile")
+    shelltools.system("wget https://raw.githubusercontent.com/pmaccarron/package.yml-files/master/multimedia/video/plexmediaserver/files/plexmediaserver.tmpfile")
     shelltools.system("mkdir tmp")
     shelltools.system("mv plexmediaserver.tmpfile tmp/plexmediaserver.conf")
 
-    shelltools.system("wget https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/plexmediaserver/files/plexmediaserver.service")
+    shelltools.system("wget https://raw.githubusercontent.com/pmaccarron/package.yml-files/master/multimedia/video/plexmediaserver/files/plexmediaserver.service")
 
 def install():
     pisitools.insinto("/opt", "usr/lib/plexmediaserver")
